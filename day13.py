fa8e849bfbea00061023a3f04e5b21f4c6842b90
Dhyeybhuva2003 <112788076+Dhyeybhuva2003@users.noreply.github.com>
2022-12-12 03:16:12 +0530

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,56 @@
+a="!!dhyey!! dhyey"
+print(len(a));
+print(a.upper());
+print(a.lower());
+print(a.rstrip("!"));
+print(a.replace("dhyey","diya"))
+print(a.split(" "));
+blogehading="intro to js"
+print(blogehading.capitalize()) 
+
+str1="welcomm to the console!!!"
+print(len(str1.center(50)))
+print(len(str1))
+print(a.count("dhyey"))
+
+print(str1.endswith("!!!"))
+print(str1.endswith("to",4,10))
+
+str2="he's name is den ,he is an honest man."
+print(str2.find("is"))
+print(str2.find("dhyey")) 
+
+print(str2.index("den"))
+# print(str2.index("dhyey"))
+
+str3="Welcometotheconsole"
+print(str3.isalnum())
+
+str4="welcomm"
+print(str4.isalpha
+      ())
+
+str5="hello word"
+print(str5.islower())
+
+str6="we wish you a merry christmas"
+print(str6.isprintable())
+
+str7="             "
+print(str7.isspace())
+str8="  "
+print(str8.isspace())
+
+str9="World Health Organization"
+print(str9.istitle())
+
+str10="WORD HEALTH ORGANIZATION"
+print(str10.isupper())
+
+str11="Python is a interpreted language"
+print(str11.startswith("Python"))
+
+print(str11.swapcase())
+
+str12="he's name is den. denis an honest man."
+print(str12.title())
